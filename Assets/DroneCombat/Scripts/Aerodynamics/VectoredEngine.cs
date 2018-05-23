17174f298c7f0e7d8cd2651f1862aaa1c92523ed
plenglin <maximupwnguin@gmail.com>
2018-05-23 13:12:22 -0700

    Script organization

---
+++ Assets/DroneCombat/Scripts/Aerodynamics/VectoredEngine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DroneCombat.Aerodynamics {
+    public class VectoredEngine : MonoBehaviour {
+
+        public Rigidbody fuselage;
+        public Vector3 maxForce;
+        public float power;
+
+        public float maxPhi;
+
+        public float theta;
+        public float phi;
+
+        private ParticleSystem ps;
+
+        private void Start() {
+            ps = GetComponent<ParticleSystem>();
+        }
+
+        private void FixedUpdate() {
+            Quaternion forceRot = Quaternion.Euler(0, 90 - phi, theta);
+            float realPower = Mathf.Clamp(power, -1, 1);
+            fuselage.AddForce(transform.rotation * maxForce * realPower);
+            if (ps != null) {
+                ps.Emit((int)(5 * realPower));
+            }
+        }
+
+    }
+}
---
+++ Assets/DroneCombat/Scripts/Aerodynamics/Wing.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DroneCombat.Aerodynamics {
+
+    public class Wing : MonoBehaviour {
+
+        public Vector3 liftAxis, liftCenter, flapAxis;
+        public float flapLimit;
+        public float flapAngle;
+        public bool canRotate;
+        public GameObject render;
+        public float drag;
+
+        public float yawInfluence, pitchInfluence, rollInfluence;
+
+        //private Vector3 prevPos;
+
+        [NonSerialized]
+        private Rigidbody rb;
+
+        public Vector3 GlobalLiftCenter {
+            get {
+                return transform.position + liftCenter;
+            }
+        }
+        public Vector3 GlobalLiftNormal {
+            get {
+                return transform.rotation * liftAxis;
+            }
+        }
+
+        public Vector3 LocalLiftAtAngle(float a) {
+            float angleClamped = Mathf.Clamp(a, -flapLimit, flapLimit);
+            return Quaternion.AngleAxis(angleClamped, flapAxis) * liftAxis;
+        }
+
+        public Vector3 GlobalLiftAtAngle(float a) {
+            return transform.rotation * LocalLiftAtAngle(a);
+        }
+
+        private void Start() {
+            //prevPos = transform.position;
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate() {
+            //Vector3 v = (transform.position - prevPos) / Time.fixedDeltaTime;
+
+            // Transform the velocities to local space
+            Vector3 v = Quaternion.Inverse(transform.rotation) * rb.velocity;
+            Vector3 v2 = v * v.magnitude;
+
+            // Perform mathy shit
+            float liftCoeff = liftAxis.magnitude;
+            Vector3 liftNorm = liftAxis / liftCoeff;
+            float angleClamped = Mathf.Clamp(flapAngle, -flapLimit, flapLimit);
+            Vector3 liftNormRot = Quaternion.AngleAxis(angleClamped, flapAxis) * liftNorm;
+
+            Vector3 liftLocalFinal = -Vector3.Dot(v2, liftNormRot) * liftNormRot * liftCoeff;
+
+            Vector3 dragLocalFinal = -v2 * this.drag;
+
+            // Transform the vectors to world space
+            Vector3 finalGlobalForce = transform.rotation * (dragLocalFinal + liftLocalFinal);
+            rb.AddForceAtPosition(finalGlobalForce, GlobalLiftCenter);
+
+            // Visual debug shit
+            //Debug.DrawLine(GlobalLiftCenter, GlobalLiftCenter + transform.rotation * liftLocalFinal / 10);
+            //Debug.DrawLine(GlobalLiftCenter, GlobalLiftCenter + GlobalLiftNormal);
+            if (canRotate) {
+                Quaternion flapRotation = Quaternion.AngleAxis(angleClamped, flapAxis);
+                render.transform.rotation = transform.rotation * flapRotation;
+            }
+            //prevPos = transform.position;
+        }
+    }
+
+}
